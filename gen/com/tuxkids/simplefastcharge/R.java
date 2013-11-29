c3d0cfaedf660adc355a0faae2406c1405ffac91
tuxkids <ukie.tux@gmail.com>
2013-11-29 14:14:34 +0700

    add to version 1.0

--- gen/com/tuxkids/simplefastcharge/R.java
+++ gen/com/tuxkids/simplefastcharge/R.java
@@ -22,10 +22,16 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_status=0x7f020001;
+        public static final int ic_status_bg=0x7f020002;
+        public static final int ic_status_off=0x7f020003;
+        public static final int ic_status_on=0x7f020004;
+        public static final int logo=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080002;
         public static final int check=0x7f080000;
+        public static final int imageView1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -70,5 +76,7 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int Widget_Button_Toggle=0x7f060002;
+        public static final int toggleButton=0x7f060003;
     }
 }
